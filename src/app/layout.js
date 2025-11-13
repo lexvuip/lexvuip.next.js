e509ae50cfcacf12d97415a70edb668900980c04
Dinesh Kumar Pokkula <dineshkumarpokkula@MacBook-Pro.local>
2025-11-13 14:57:56 +0530

    made modification in layout.js ifle

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "../components/NavBar";
-import { Icons } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
