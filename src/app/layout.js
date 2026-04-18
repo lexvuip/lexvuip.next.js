7467ce904992c8e7e1dafd5d639ebdb744c735f0
dineshnikhil <dineshkumarpokkula@gmail.com>
2026-04-18 18:17:29 +0530

    perf,a11y: optimize font loading and enhance accessibility
    - Remove render-blocking Google Fonts CSS from layout and globals
    
    - Fix WCAG color contrast ratios for gold text elements
    
    - Add descriptive screen-reader text to generic link buttons
    
    - Correct footer heading hierarchy (h4 to h2)
    
    - Fix 404 error by adding missing about_video_poster.jpg

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,10 +38,6 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="preconnect" href="https://images.unsplash.com" />
         <link rel="dns-prefetch" href="https://images.unsplash.com" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
-        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="dns-prefetch" href="https://fonts.gstatic.com" />
       </head>
       <body className={`${playfair.variable} ${manrope.variable}`} suppressHydrationWarning>
         <NavBar />
